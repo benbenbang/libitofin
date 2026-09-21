268f2ed70f17e16a91d6053c37d3d0dbf3df5340
benbenbang <bn@bitbrew.dev>
2026-09-21 10:41:28 +0800

    feat(libitofin): add overnight future rate helpers
    ref #808

--- crates/libitofin/src/termstructures/yields/mod.rs
+++ crates/libitofin/src/termstructures/yields/mod.rs
@@ -58,3 +58,6 @@ pub(super) fn sync_extrapolation(
         }
     }
 }
+
+pub mod overnightfutureratehelpers;
+pub use overnightfutureratehelpers::{OvernightIndexFutureRateHelper, SofrFutureRateHelper};
